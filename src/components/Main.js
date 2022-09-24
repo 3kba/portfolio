76ea131f62d045d941f291a9d57d34b7c0250a04
3kba <9kbabouziane@gmail.com>
2022-09-24 11:03:25 +0100

    updated

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,95 +1,86 @@
-import { motion } from 'framer-motion'
-import React, { useState } from 'react'
-import { NavLink } from 'react-router-dom'
-import styled, { keyframes } from 'styled-components'
-import LogoComponent from '../subComponents/LogoComponent'
-import PowerButton from '../subComponents/PowerButton'
-import SocialIcons from '../subComponents/SocialIcons'
-import { YinYang } from './AllSvgs'
-import Intro from './Intro'
-import {Toaster, toast} from 'react-hot-toast'
-import { color } from '@mui/system'
-
-
+import { motion } from "framer-motion";
+import React, { useState } from "react";
+import { NavLink } from "react-router-dom";
+import styled, { keyframes } from "styled-components";
+import LogoComponent from "../subComponents/LogoComponent";
+import PowerButton from "../subComponents/PowerButton";
+import SocialIcons from "../subComponents/SocialIcons";
+import { YinYang } from "./AllSvgs";
+import Intro from "./Intro";
+import { Toaster, toast } from "react-hot-toast";
+import { color } from "@mui/system";
 
 const MainContainer = styled(motion.div)`
+  background: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  overflow: hidden;
+  position: relative;
 
-background: ${props => props.theme.body};
-width: 100vw;
-height: 100vh;
-overflow: hidden;
-position: relative;
-
-h2,h3,h4,h5,h6{
-  font-family: 'Karla', sans-serif;
-  font-weight: 500;
-
-}
-
-`
+  h2,
+  h3,
+  h4,
+  h5,
+  h6 {
+    font-family: "Karla", sans-serif;
+    font-weight: 500;
+  }
+`;
 
 const Container = styled.div`
-
-padding: 2rem;
-
-
-`
+  padding: 2rem;
+`;
 
 const Contact = styled.a`
-color :${props => props.theme.text};
-position: absolute;
-top: 2rem;
-right: calc(1rem + 2vw);
-text-decoration: none;
-z-index: 1;
-
-`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 2rem;
+  right: calc(1rem + 2vw);
+  text-decoration: none;
+  z-index: 1;
+`;
 
 const Blog = styled(NavLink)`
-color :${props => props.theme.text};
-position: absolute;
-top: 50%;
-right: calc(1rem + 2vw);
-transform:rotate(90deg) translate(-50%,-50%);
-text-decoration: none;
-z-index: 1;
-
-`
+  color: ${(props) => props.theme.text};
+  position: absolute;
+  top: 50%;
+  right: calc(1rem + 2vw);
+  transform: rotate(90deg) translate(-50%, -50%);
+  text-decoration: none;
+  z-index: 1;
+`;
 
 const Work = styled(NavLink)`
-color: ${props => props.click ? props.theme.body : props.theme.text};
-position: absolute;
-top: 50%;
-left: calc(1rem + 2vw);
-transform: translate(-50%,-50%) rotate(-90deg);
-text-decoration: none;
-z-index: 1;
-
-`
+  color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
+  position: absolute;
+  top: 50%;
+  left: calc(1rem + 2vw);
+  transform: translate(-50%, -50%) rotate(-90deg);
+  text-decoration: none;
+  z-index: 1;
+`;
 
 const BottomBar = styled.div`
-position: absolute;
-bottom: 1rem;
-left: 0;
-right: 0;
-width: 100%;
+  position: absolute;
+  bottom: 1rem;
+  left: 0;
+  right: 0;
+  width: 100%;
 
-display: flex;
-justify-content: space-evenly; 
-`
+  display: flex;
+  justify-content: space-evenly;
+`;
 
 const About = styled(NavLink)`
-color: ${props => props.click ? props.theme.body : props.theme.text};
-text-decoration: none;
-z-index: 1;
-
-`
+  color: ${(props) => (props.click ? props.theme.body : props.theme.text)};
+  text-decoration: none;
+  z-index: 1;
+`;
 
 const Skills = styled(NavLink)`
-color :${props => props.theme.text};
-z-index: 1;
-
-`
+  color: ${(props) => props.theme.text};
+  z-index: 1;
+`;
 
 const rotate = keyframes`
 from{
@@ -99,46 +90,45 @@ from{
 
 }
 
-`
+`;
 
 const Center = styled.button`
-position: absolute;
-top: ${props => props.click ? '85%' : '50%'};
-left: ${props => props.click ? '92%' : '50%'};
-transform: translate(-50%,-50%);
-border: none;
-outline: none;
-background-color: transparent;
-cursor: pointer;
+  position: absolute;
+  top: ${(props) => (props.click ? "85%" : "50%")};
+  left: ${(props) => (props.click ? "92%" : "50%")};
+  transform: translate(-50%, -50%);
+  border: none;
+  outline: none;
+  background-color: transparent;
+  cursor: pointer;
 
-display: flex;
-flex-direction: column;
-justify-content: center;
-align-items: center;
-transition: all 1s ease;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  transition: all 1s ease;
 
-&>:first-child{
-  animation : ${rotate} infinite 1.5s linear;
-}
+  & > :first-child {
+    animation: ${rotate} infinite 1.5s linear;
+  }
 
-
-&>:last-child{
-  display: ${props => props.click ? 'none' : 'inline-block'};
-  padding-top: 1rem;
-}
-`
+  & > :last-child {
+    display: ${(props) => (props.click ? "none" : "inline-block")};
+    padding-top: 1rem;
+  }
+`;
 
 const DarkDiv = styled.div`
-position:absolute;
-top: 0;
-background-color: #000;
-bottom: 0;
-right: 50%;
-width: ${props => props.click ? '50%' : '0%'};
-height: ${props => props.click ? '100%' : '0%'};
-z-index: 1;
-transition: height 0.5s ease, width 1s ease 0.5s;
-`
+  position: absolute;
+  top: 0;
+  background-color: #000;
+  bottom: 0;
+  right: 50%;
+  width: ${(props) => (props.click ? "50%" : "0%")};
+  height: ${(props) => (props.click ? "100%" : "0%")};
+  z-index: 1;
+  transition: height 0.5s ease, width 1s ease 0.5s;
+`;
 
 // const down = {
 //   open: {
@@ -167,165 +157,174 @@ transition: height 0.5s ease, width 1s ease 0.5s;
 //   }
 // }
 
-// const variants = 
+// const variants =
 
 const Main = () => {
-  const [click, setClick] = useState(false)
-  const handleClick = () => setClick(!click)
-  
-  
-  const handleAnimations = (updatedVariants) => setVariants(updatedVariants) 
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+
+  const handleAnimations = (updatedVariants) => setVariants(updatedVariants);
 
   const [variants, setVariants] = useState({
-        hidden: {opacity: 0},
-        visible: { opacity: 1, y: 0 },
-        exit: {opacity: 1, y: '-100vh'},
-        transition: {ease:"circOut",type:"tween",duration: 1},
-  })
+    hidden: { opacity: 0 },
+    visible: { opacity: 1, y: 0 },
+    exit: { opacity: 1, y: "-100vh" },
+    transition: { ease: "circOut", type: "tween", duration: 1 },
+  });
 
   // const [isOpen, setIsOpen] = useState(false)
 
   return (
-          // initial={{opacity: 0 }}
-          // animate={{opacity: 1, y: 0}}
-          // exit={{opacity: 0, y: '-100vh'}}
-          // transition={{ease:"circOut",type:"tween",duration: 0.5}}
-          // initial={{opacity: 0}}
-          // animate={{opacity: 1, x: 0}}
-          // exit={{opacity: 1, x: '100vw'}}
-          // transition={{ease:"circOut",type:"tween",duration: 1}}
-          <MainContainer
-          initial='hidden'
-          animate='visible'
-          exit='exit'
-          transition='transition'
-          variants={variants}
+    // initial={{opacity: 0 }}
+    // animate={{opacity: 1, y: 0}}
+    // exit={{opacity: 0, y: '-100vh'}}
+    // transition={{ease:"circOut",type:"tween",duration: 0.5}}
+    // initial={{opacity: 0}}
+    // animate={{opacity: 1, x: 0}}
+    // exit={{opacity: 1, x: '100vw'}}
+    // transition={{ease:"circOut",type:"tween",duration: 1}}
+    <MainContainer
+      initial="hidden"
+      animate="visible"
+      exit="exit"
+      transition="transition"
+      variants={variants}
+    >
+      <DarkDiv click={click} />
+      <Container>
+        <PowerButton />
+        <LogoComponent theme={click ? "dark" : "light"} />
+        <SocialIcons theme={click ? "dark" : "light"} />
+        <Center click={click}>
+          <YinYang
+            onClick={() => handleClick()}
+            width={click ? 120 : 200}
+            height={click ? 120 : 200}
+            fill="currentColor"
+          />
+          <span>Click Here</span>
+        </Center>
+
+        <Contact
+          href="mailto:9kbabouziane@gmail.com"
+          rel="noreferrer"
+          target="_blank"
+        >
+          <motion.h2
+            initial={{
+              y: -200,
+              transition: { type: "spring", duration: 1.5, delay: 1 },
+            }}
+            animate={{
+              y: 0,
+              transition: { type: "spring", duration: 1.5, delay: 1 },
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
           >
-        <DarkDiv click={click} />
-        <Container>
-            <PowerButton />
-            <LogoComponent theme={click ? 'dark' : 'light' } />
-            <SocialIcons theme={click ? 'dark' : 'light'} />
-            <Center click={click}>
-              <YinYang onClick={() => handleClick()} width={click ? 120 : 200} height={click ? 120 : 200} fill='currentColor' />
-              <span>Click Here</span>
-            </Center>
+            Say hi..
+          </motion.h2>
+        </Contact>
 
-            <Contact href='mailto:9kbabouziane@gmail.com' rel="noreferrer" target='_blank'>
-              
-              <motion.h2
-              initial={{
-                y:-200,
-                transition : {type: 'spring', duration: 1.5, delay:1}
-              }}
-              animate={{
-                y:0,
-                transition : {type: 'spring', duration: 1.5, delay:1}
-              }}
-              whileHover={{scale: 1.1}}
-              whileTap={{scale: 0.9}}
-
-              >
-                Say hi..
-              </motion.h2>
-
-            </Contact>
-
-            <Blog to =''>
-              
-              <motion.h2
-              onClick={() => toast("Coming Soon.." , {
+        <Blog to="">
+          <motion.h2
+            onClick={() =>
+              toast("Coming Soon..", {
                 duration: 3000,
-                position: 'top-center',
-                style: {background : "#FCF6F4",
-                        color: '#000'},
-              })}
+                position: "top-center",
+                style: { background: "#FCF6F4", color: "#000" },
+              })
+            }
+            initial={{
+              y: -200,
+              transition: { type: "spring", duration: 1.5, delay: 1 },
+            }}
+            animate={{
+              y: 0,
+              transition: { type: "spring", duration: 1.5, delay: 1 },
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Blog
+          </motion.h2>
+        </Blog>
+        <Toaster />
+        <Work
+          to="/"
+          // onClick={() =>
+          //   handleAnimations({
+          //     ...variants,
+          //     exit: { opacity: 0, x: "100vw" },
+          //     visible: { opacity: 1, x: 0 },
+          //   })
+          // }
+          onClick={() =>
+            toast("Coming Soon..", {
+              duration: 3000,
+              position: "top-center",
+              style: { background: "#FCF6F4", color: "#000" },
+            })
+          }
+          click={click}
+          // click={() => {
+          //   setVariants({...variants, exit :{opacity : 0, y:'100vh'}, visible: { opacity: 1, y: 0 }});
+          //   click();
+          // }}
+        >
+          <motion.h2
+            initial={{
+              y: -200,
+              transition: { type: "spring", duration: 1.5, delay: 1 },
+            }}
+            animate={{
+              y: 0,
+              transition: { type: "spring", duration: 1.5, delay: 1 },
+            }}
+            whileHover={{ scale: 1.1 }}
+            whileTap={{ scale: 0.9 }}
+          >
+            Work
+          </motion.h2>
+        </Work>
+        <BottomBar>
+          <About to="/about" click={click}>
+            <motion.h2
               initial={{
-                y:-200,
-                transition : {type: 'spring', duration: 1.5, delay:1}
+                y: 200,
+                transition: { type: "spring", duration: 1.5, delay: 1 },
               }}
               animate={{
-                y:0,
-                transition : {type: 'spring', duration: 1.5, delay:1}
-              }} 
-              whileHover={{scale: 1.1}}
-              whileTap={{scale: 0.9}}
-              >
-                Blog
-              </motion.h2>
-              
-            </Blog>
-            <Toaster 
-              />
-            <Work  to='/work' 
-            onClick={() => handleAnimations({...variants, exit :{opacity : 0, x:'100vw'}, visible: { opacity: 1, x: 0 } })} 
-            click={click}  
-            // click={() => {
-            //   setVariants({...variants, exit :{opacity : 0, y:'100vh'}, visible: { opacity: 1, y: 0 }});
-            //   click();
-            // }}
+                y: 0,
+                transition: { type: "spring", duration: 1.5, delay: 1 },
+              }}
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
             >
-              
-              <motion.h2
+              About.
+            </motion.h2>
+          </About>
+          <Skills to="/skills" click={click}>
+            <motion.h2
               initial={{
-                y:-200,
-                transition : {type: 'spring', duration: 1.5, delay:1}
+                y: 200,
+                transition: { type: "spring", duration: 1.5, delay: 1 },
               }}
               animate={{
-                y:0,
-                transition : {type: 'spring', duration: 1.5, delay:1}
+                y: 0,
+                transition: { type: "spring", duration: 1.5, delay: 1 },
               }}
-              whileHover={{scale: 1.1}}
-              whileTap={{scale: 0.9}}
-              >
-                Work
-              </motion.h2>
-
-            </Work>
-            <BottomBar>
-            <About to='/about' click={click} >
-              
-              <motion.h2
-              initial={{
-                y:200,
-                transition : {type: 'spring', duration: 1.5, delay:1}
-              }}
-              animate={{
-                y:0,
-                transition : {type: 'spring', duration: 1.5, delay:1}
-              }}
-              whileHover={{scale: 1.1}}
-              whileTap={{scale: 0.9}}
-              >
-                About.
-              </motion.h2>
-
-            </About>
-            <Skills to='/skills' click={click}>
-              
-              <motion.h2
-              initial={{
-                y:200,
-                transition : {type: 'spring', duration: 1.5, delay:1}
-              }}
-              animate={{
-                y:0,
-                transition : {type: 'spring', duration: 1.5, delay:1}
-              }}
-              whileHover={{scale: 1.1}}
-              whileTap={{scale: 0.9}}
-              >
-                My Skills.
-              </motion.h2>
-
-            </Skills>
-            </BottomBar>
-
-
-        </Container>
-        {click ? <Intro click={click} /> : null} 
+              whileHover={{ scale: 1.1 }}
+              whileTap={{ scale: 0.9 }}
+            >
+              My Skills.
+            </motion.h2>
+          </Skills>
+        </BottomBar>
+      </Container>
+      {click ? <Intro click={click} /> : null}
     </MainContainer>
-  )
-}
+  );
+};
 
-export default Main
+export default Main;
--- src/components/MySkillsPage.js
+++ src/components/MySkillsPage.js
@@ -1,113 +1,101 @@
-import React from 'react'
-import styled, { ThemeProvider } from 'styled-components'
-import { LightTheme } from './Theme'
-import {Design ,Develope } from './AllSvgs';
-import PowerButton from '../subComponents/PowerButton';
-import LogoComponent from '../subComponents/LogoComponent';
-import SocialIcons from '../subComponents/SocialIcons';
-import ParticleComponent from '../subComponents/ParticleComponent';
+import React from "react";
+import styled, { ThemeProvider } from "styled-components";
+import { LightTheme } from "./Theme";
+import { Design, Develope } from "./AllSvgs";
+import PowerButton from "../subComponents/PowerButton";
+import LogoComponent from "../subComponents/LogoComponent";
+import SocialIcons from "../subComponents/SocialIcons";
+import ParticleComponent from "../subComponents/ParticleComponent";
 
-import { motion } from 'framer-motion';
-
-
+import { motion } from "framer-motion";
 
 const Box = styled(motion.div)`
-background-color: ${props => props.theme.body};
-width: 100vw;
-height: 100vh;
-position: relative;
-display: flex;
-justify-content:center;
-align-items: center;
-
-
-
-`
-
+  background-color: ${(props) => props.theme.body};
+  width: 100vw;
+  height: 100vh;
+  position: relative;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+`;
 
 const Main = styled.div`
+  border: 2px solid ${(props) => props.theme.text};
+  color: ${(props) => props.theme.text};
+  background-color: ${(props) => props.theme.body};
+  padding: 2rem;
+  width: 30vw;
+  height: 60vh;
+  z-index: 3;
+  line-height: 1.5;
 
-border: 2px solid ${props => props.theme.text};
-color: ${props => props.theme.text};
-background-color: ${props => props.theme.body};
-padding: 2rem;
-width: 30vw;
-height: 60vh;
-z-index: 3;
-line-height: 1.5;
+  font-family: "Ubuntu Mono", monospace;
+  display: flex;
+  flex-direction: column;
+  justify-content: space-between;
 
-font-family: 'Ubuntu Mono', monospace;
-display: flex;
-flex-direction: column;
-justify-content: space-between;
-
-&:hover{
-  color: ${props => props.theme.body};
-  background-color: ${props => props.theme.text} ;
-}
-`
+  &:hover {
+    color: ${(props) => props.theme.body};
+    background-color: ${(props) => props.theme.text};
+  }
+`;
 
 const Title = styled.h1`
-display: flex;
-justify-content: center;
-align-items: center;
-font-size: calc(2em +2vw);
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  font-size: calc(2em +2vw);
 
-&>*:first-child{
-  margin-right: 1rem;
-}
-${Main}:hover &{
-  &>*{
-    fill: ${props => props.theme.body};
+  & > *:first-child {
+    margin-right: 1rem;
   }
-
-}
-
-
-`
+  ${Main}:hover & {
+    & > * {
+      fill: ${(props) => props.theme.body};
+    }
+  }
+`;
 
 const Description = styled.div`
-color: ${props => props.theme.text};
-font-size: calc(0.4em + 1vw);
-padding: 0.5rem 0 ;
+  color: ${(props) => props.theme.text};
+  font-size: calc(0.4em + 1vw);
+  padding: 0.5rem 0;
 
-${Main}:hover &{
+  ${Main}:hover & {
+    color: ${(props) => props.theme.body};
+  }
 
-    color: ${props => props.theme.body};
-}
-
-strong{
-  margin-bottom: 1rem;
-  text-transform: uppercase;
-}
-ul, p{
-  margin-left: 2rem;
-}
-
-`
+  strong {
+    margin-bottom: 1rem;
+    text-transform: uppercase;
+  }
+  ul,
+  p {
+    margin-left: 2rem;
+  }
+`;
 
 const variants = {
-  hidden: {opacity: 1, y: 0},
+  hidden: { opacity: 1, y: 0 },
   visible: { opacity: 1 },
-  exit: {opacity: 1, y: '100vh'},
-  transition: {ease:"circOut",type:"tween",duration: 1},
-}
+  exit: { opacity: 1, y: "100vh" },
+  transition: { ease: "circOut", type: "tween", duration: 1 },
+};
 
 const MySkillsPage = () => {
   return (
     <ThemeProvider theme={LightTheme}>
-      <Box 
-          initial='hidden'
-          animate='visible'
-          exit='exit'
-          transition='transition'
-          variants={variants}
-          
-          >
-            <LogoComponent />
-            <SocialIcons theme={'light'} />
-            <PowerButton theme={'light' }  />
-            <ParticleComponent theme='light' />
+      <Box
+        initial="hidden"
+        animate="visible"
+        exit="exit"
+        transition="transition"
+        variants={variants}
+      >
+        <LogoComponent />
+        <SocialIcons theme={"light"} />
+        <PowerButton theme={"light"} />
+        <ParticleComponent theme="light" />
         {/* <Main>
           <Title>
               <Design width={40} height={40} /> Desginer
@@ -120,25 +108,27 @@ const MySkillsPage = () => {
         </Main> */}
         <Main>
           <Title>
-              <Develope width={50} height={50} /> Frontend Developer
+            <Develope width={50} height={50} /> Frontend Developer
           </Title>
           <Description>
-              I value business or brand for which i'm creating, thus i enjoy bringing new ideas to life.
-               
+            I value business or brand for which i'm creating, thus i enjoy
+            bringing new ideas to life.
           </Description>
           <Description>
-              <strong>Skills</strong>
-              <p>Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase etc.</p>
+            <strong>Skills</strong>
+            <p>
+              Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase
+              etc.
+            </p>
           </Description>
           <Description>
-              <strong>Tools</strong>
-              <p>Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase etc.</p>
+            {/* <strong>Tools</strong>
+              <p>Html, Css, Js, React, Redux, Sass, Bootstrap, Tailwind, Firebase etc.</p> */}
           </Description>
         </Main>
-        
       </Box>
     </ThemeProvider>
-  )
-}
+  );
+};
 
-export default MySkillsPage
+export default MySkillsPage;
